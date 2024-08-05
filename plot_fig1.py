e35b7340580e113e9c91ef394e52654ae3724c83
Robyn Stuart <robyna.s@gmail.com>
2024-08-06 08:33:23 +1000

    finish impact fig

--- plot_fig1.py
+++ plot_fig1.py
@@ -8,7 +8,7 @@ import sciris as sc
 import numpy as np
 
 
-def plot_single(ax, mres, to_plot, si, ei, color, label=None, smooth=True):
+def plot_single(ax, mres, to_plot, si, ei, color, ls='-', label=None, smooth=True):
     years = mres.year[si:ei]
     best = mres[to_plot][si:ei]
     low = mres[to_plot].low[si:ei]
@@ -20,7 +20,7 @@ def plot_single(ax, mres, to_plot, si, ei, color, label=None, smooth=True):
         high = np.convolve(list(high), np.ones(5), "valid")/5
         years = years[4:]
 
-    ax.plot(years, best, color=color, label=label)
+    ax.plot(years, best, color=color, label=label, ls=ls)
     ax.fill_between(years, low, high, alpha=0.1, color=color)
     return ax
 
@@ -28,21 +28,24 @@ def plot_single(ax, mres, to_plot, si, ei, color, label=None, smooth=True):
 def plot_fig1():
 
     fig, ax = pl.subplots(1, 1, figsize=(10, 6))
-    resname = 'cancers'
-    colors = sc.gridcolors(4)
+    resname = 'asr_cancer_incidence'
 
     # What to plot
     start_year = 2016
     end_year = 2100
 
     msim_dict = sc.loadobj('results/st_scens.obj')
+    vc = sc.vectocolor(3)
+    colors = ['k'] + [vc[0], vc[0], vc[1], vc[1], vc[2], vc[2]]
     mbase = msim_dict['Baseline']
     si = sc.findinds(mbase.year, start_year)[0]
     ei = sc.findinds(mbase.year, end_year)[0]
 
     cn = 0
     for slabel, mres in msim_dict.items():
-        ax = plot_single(ax, mres, resname, si, ei, color=colors[cn], label=slabel)
+        if '50/90' in slabel: ls = '--'
+        else: ls = '-'
+        ax = plot_single(ax, mres, resname, si, ei, color=colors[cn], ls=ls, label=slabel)
         cn += 1
 
     ax.set_ylim(bottom=0)  #, top=23)
