9c1984e88137c8917d71dc31448378e39787bd3f
jcdusingize <160474442+jcdusingize@users.noreply.github.com>
2025-04-29 13:20:38 +1000

    Update run_sim_HIV.py

--- run_sim_HIV.py
+++ run_sim_HIV.py
@@ -1,5 +1,5 @@
 """
-Define the HPVsim simulation
+Define the HPVsim simulation for Rwanda
 """
 
 # Standard imports
@@ -7,6 +7,7 @@ import pylab as pl
 import numpy as np
 import sciris as sc
 import hpvsim as hpv
+import pandas as pd
 
 from interventions import make_st, make_vx
 
@@ -21,7 +22,7 @@ save_plots = True
 
 
 # %% Simulation creation functions
-def make_sim(location='rwanda', calib_pars=None, debug=0, add_vax=True, add_st=True, interventions=None, analyzers=None, datafile=None, seed=1, end=2025):
+def make_sim(location='rwanda', calib_pars=None, debug=0, add_vax=True, add_st=True, interventions=None, analyzers=None, datafile=None, seed=1, end=2025, hiv_datafile=None, art_datafile=None):
     """
     Define parameters, analyzers, and interventions for the simulation
     """
@@ -36,23 +37,25 @@ def make_sim(location='rwanda', calib_pars=None, debug=0, add_vax=True, add_st=T
         location=location,
         init_hpv_dist=dict(hpv16=0.4, hpv18=0.25, hi5=0.25, ohr=.1),
         init_hpv_prev={
-            'age_brackets': np.array([12, 17, 24, 34, 44, 64, 80, 150]),
+            'age_brackets': np.array([12, 17, 24, 34, 44, 64, 80, 150]),     # HPV PREVALENCE FROMNM WHICH COUNTRY ?
             'm': np.array([0.0, 0.25, 0.6, 0.25, 0.05, 0.01, 0.0005, 0]),
             'f': np.array([0.0, 0.35, 0.7, 0.25, 0.05, 0.01, 0.0005, 0]),
         },
-        ms_agent_ratio=100,
-        verbose=0.0,
+        ms_agent_ratio=100,                 #for every 1 "real" cancer case, 100 "normal" agents are simulated
+        verbose=0.0,                        # the model runs silently without printing messages
         rand_seed=seed,
+        model_hiv=True,
+        hiv_pars=dict(),
     )
 
     # Sexual behavior parameters
     # Debut: derived by fitting to 2019-20 DHS
     # Women:
-    #           Age:  15,   18,   20,   22,   25
-    #   Prop_active: 2.1, 19.8, 41.7, 62.9, 81.7
+              Age:  15,   18,   20,   22,   25
+       Prop_active: 2.1, 19.8, 41.7, 62.9, 81.7
     # Men:
-    #           Age:  15,   18,   20,   22,   25
-    #   Prop_active: 2.7, 14.4, 30.6, 47.6, 69.8
+               Age:  15,   18,   20,   22,   25
+        Prop_active: 2.7, 14.4, 30.6, 47.6, 69.8
     # For fitting, see https://www.researchsquare.com/article/rs-3074559/v1
     pars.debut = dict(
         f=dict(dist='lognormal', par1=20.96, par2=3.34),
@@ -86,6 +89,13 @@ def make_sim(location='rwanda', calib_pars=None, debug=0, add_vax=True, add_st=T
         c=dict(dist='poisson1', par1=0.2),
     )
 
+    # sev_dist parameters
+    pars.sev_dist = dict(dist='normal_pos', par1=1.33, par2=0.2)
+
+    # HIV parameters
+    pars.hiv_pars['art_failure_prob'] = 0.1
+
+ # If calibration parameters have been supplied, use them here
     if calib_pars is None:
         # Use defaults
         calib_pars = sc.loadobj(f'results/{location}_pars.obj')
@@ -100,16 +110,28 @@ def make_sim(location='rwanda', calib_pars=None, debug=0, add_vax=True, add_st=T
         interventions = sc.autolist(interventions)
         interventions += make_st(end_year=end)
 
-    sim = hpv.Sim(pars=pars, interventions=interventions, analyzers=analyzers, datafile=datafile)
-
+# Create the sim
+    sim = hpv.Sim(pars=pars, interventions=interventions, analyzers=analyzers, rand_seed=seed, datafile=datafile,hiv_datafile=hiv_datafile, art_datafile=art_datafile)
     return sim
-
+                 # If you create "sim = hpv.Sim()" ONLY, it will create a Sim object with the DEFAULTS values.
 
 # %% Simulation running functions
 def run_sim(
         analyzers=None, interventions=None, debug=0, seed=1, verbose=0.2,
-        do_save=False, end=2020, add_vax=True, add_st=True, calib_pars=None, meta=None):
+        do_save=False, end=2020, add_vax=True, add_st=True, calib_pars=None, meta=None, hiv_datafile=None, art_datafile=None):
 
+ # Make arguments                                # WHY THESE DATASETS ARE DEFINED HERE WHILE THEY ARE DEFINED IN CALIBRATION SCRIPT?
+    if hiv_datafile is None:
+        hiv_datafile = ['data/hiv_incidence_rwanda.csv', 
+                        'data/rwanda_hiv_mortality_by_age_females.csv',
+                        'data/rwanda_hiv_mortality_by_age_males.csv']
+        
+    if art_datafile is None:         
+         # art_datafile = ['data/rwanda_art_coverage_by_age_males.csv',
+         #                 'data/rwanda_art_coverage_by_age_females.csv']
+         art_datafile = ['rwanda_ART_coverage.csv']
+                                                # WHY CANCER DATASETS ARENT DEFINED HERE? 
+            
     # Make sim
     sim = make_sim(
         debug=debug,
@@ -117,6 +139,8 @@ def run_sim(
         add_st=add_st,
         interventions=interventions,
         analyzers=analyzers,
+        hiv_datafile=hiv_datafile,
+        art_datafile=art_datafile,
         calib_pars=calib_pars,
         end=end,
     )
@@ -133,8 +157,8 @@ def run_sim(
 
     # Run
     sim['verbose'] = verbose
-    sim.run()
-    sim.shrink()
+    sim.run()        # Executes the simulation	Always — required to generate output
+    sim.shrink()     # Minimizes memory by trimming details	Optional — for cleanup or saving
 
     if do_save:
         sim.save(f'results/{location}.sim')
@@ -143,7 +167,7 @@ def run_sim(
 
 
 # %% Run as a script
-if __name__ == '__main__':
+if __name__ == '__main__':            # This is Python's standard way of saying: Only run the following code if this file is executed directly.But if another script imports this file (e.g., import my_script), the code in this block won’t run — which prevents unwanted side effects.
 
     T = sc.timer()
 
