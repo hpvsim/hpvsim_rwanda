2b49d2456fa05482d21841098f7a1f18e5c6a361
Robyn Stuart <robyna.s@gmail.com>
2025-11-25 13:56:45 +1100

    test 25%

--- interventions.py
+++ interventions.py
@@ -25,7 +25,7 @@ def make_male_vx(prob=None):
 
 
 def make_st(primary='hpv', prev_screen_cov=0.1, future_screen_cov=0.4, screen_change_year=2025, age_range=[30, 50],
-            start_year=2020, end_year=2100, future_treat_cov=0.75, txv_pars=None, txv=False,
+            start_year=2020, end_year=2100, future_treat_cov=0.25, txv_pars=None, txv=False,
             tx_assigner_csv='tx_assigner'):
     """
     Make screening and treatment interventions
@@ -66,7 +66,7 @@ def make_st(primary='hpv', prev_screen_cov=0.1, future_screen_cov=0.4, screen_ch
     # future_triage_cov = 0.9
     # triage_prob = np.array([prev_treat_cov]*n_prev_years+[future_triage_cov]*n_future_screen_years)
     triage_prob = np.array([0.9]*len(triage_years))
-    n_future_years = len(future_years)
+    # n_future_years = len(future_years)
 
     tx_assigner = hpv.default_dx('tx_assigner')
     tx_assigner.df = pd.read_csv(f'{tx_assigner_csv}.csv')  # 60/70 sens/spec if using VIA
